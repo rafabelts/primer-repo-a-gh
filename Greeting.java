5632beb1740822708280e942ca7b04db62b9f9a8
Rafael Beltran <rafabeltrans17@gmail.com>
2025-08-28 15:35:41 -0600

    ask for user's age

--- Greeting.java
+++ Greeting.java
@@ -4,15 +4,17 @@ public class Greeting {
 
   public static void main(String[] args) {
 
-    System.out.print("Write your input: ");
+    System.out.print("Write your name: ");
     Scanner scanner = new Scanner(System.in);
     String userInput = scanner.next();
 
-    for (int i = 0; i < 3; i++) {
-      System.out.println(userInput);
-    }
+    System.out.print("Write your age: ");
+    Integer age = scanner.nextInt();
 
     scanner.close();
-  }
 
+    if (age >= 18) {
+      System.out.println("Congratulations " + userInput + " you can vote!");
+    }
+  }
 }
